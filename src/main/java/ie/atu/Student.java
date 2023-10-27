4ad278cb1ba88d87879a7b43a142ddbf71030103
AlexP8on <118891272+AlexP8on@users.noreply.github.com>
2023-10-27 18:10:43 +0100

    Created Three instances of Student Class and added private instance variables: name, email, and course.

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -1,4 +1,17 @@
 package ie.atu;
 
 public class Student {
+    private String name;
+    private String email;
+    private String course;
+
+    // Constructors
+    public Student() {
+        // Default constructor
+        this.name = "";
+        this.email = "";
+        this.course = "";
+    }
+
 }
+
